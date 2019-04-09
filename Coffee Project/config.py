956bd5f91be4a2a55e4f73e9d66f8b802392ee16
Jade Belton <jadetsb@gmail.com>
2019-04-09 18:29:01 -0400

    first upload for coffee pulls

---
+++ Coffee Project/config.py
@@ -0,0 +1,19 @@
+# AWS Database Info
+remote_db_endpoint = 'codingbootcamp.cfu2z2iqlnfl.us-east-2.rds.amazonaws.com'
+remote_db_port = '3306'
+remote_gwsis_dbname = 'coffee'
+remote_gwsis_dbuser = 'root'
+remote_gwsis_dbpwd = 'Shadow08'
+
+# local Database Info
+local_gwsis_dbname = 'coffee'
+local_gwsis_dbuser = 'root'
+local_gwsis_dbpwd = 'Sh@die08'
+
+# Salesforce 
+sf_grant_type = 'password'
+sf_client_id = '' # Consumer Key
+sf_client_secret = '' # Consumer Secret
+sf_username = '' # The email you use to login
+sf_password = '' # Concat your password and your security token
+sf_endpoint = 'https://login.salesforce.com/services/oauth2/token'
