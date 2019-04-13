2a93addbac4b0a56e5f77433ad43b63c0dd66705
ChalecaDoyle <40638401+ChalecaDoyle@users.noreply.github.com>
2019-04-13 10:40:04 -0400

    Add files via upload

---
+++ Project 2 Coffee and Tea/config-1.js
@@ -0,0 +1,2 @@
+// API key
+const API_KEY = "pk.eyJ1IjoibGVjYSIsImEiOiJjanVkYjhyc3cweDVpNDNydm03aXgxZzZ0In0.oSFh8B49nBX5jHKMf0liYA";
---
+++ Project 2 Coffee and Tea/data.js
@@ -0,0 +1,32 @@
+var data = {
+  Price: [
+    2.13,
+    2.14,
+    4.79,
+    2.26
+  ],
+  Caffeine_Level: [
+    
+    180,
+    340,
+    70, 
+    60, 
+
+  ],
+  Coffee_Type: [
+   
+    Arabica,
+    Robusta,
+    Robusta,
+    Robusta,
+    Robusta,
+    Liberica,
+    Liberica,
+    Liberica,
+    Excelsa
+
+  ],
+  year: [
+    2019
+  ]
+};
